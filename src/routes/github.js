39f99afaad5564a902990ae158751e4f26e6fba3
backmeupplz <backmeupplz@gmail.com>
2017-06-25 23:35:59 -0700

    fix payload

--- src/routes/github.js
+++ src/routes/github.js
@@ -14,7 +14,7 @@ module.exports.create = create
 GitHub.prototype.post = function (req, res) {
   var authorizedIps = config.security.authorizedIps
   var githubIps = config.security.githubIps
-  var payload = req.body
+  var payload = JSON.parse(req.body.payload)
 
   if (!payload) {
     console.log('No payload')
